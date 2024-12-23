4e6103ad514656eb475fe1dbe80f1a9a12f34e5c
Lemonade <57147319+Lemonaeds@users.noreply.github.com>
2024-12-23 06:13:24 +0000

    Creacion de los get de planetas

--- src/app.py
+++ src/app.py
@@ -1,14 +1,14 @@
 """
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
-import os
+import os, json
 from flask import Flask, request, jsonify, url_for
 from flask_migrate import Migrate
 from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, User, Planet, Person, Favorites
 #from models import Person
 
 app = Flask(__name__)
@@ -36,14 +36,86 @@ def handle_invalid_usage(error):
 def sitemap():
     return generate_sitemap(app)
 
+
+#Getting the list of all users within the system.
 @app.route('/user', methods=['GET'])
-def handle_hello():
+def get_all_users():
+    try:
+        all_users = User.query.all()
+        if len(all_users) < 1:
+            return jsonify({"msg": "There are no users registered in the platform."}), 400
+        
+        serialized_users = list(map(lambda x: x.serialize(), all_users))
+        return serialized_users, 200
+    except Exception as error:
+        return jsonify({"msg": "Server error", "error":str(error)}), 500
+    
+#Getting the list of all users within the system.
+@app.route('/user/<int:user_id>', methods=['GET'])
+def get_a_user(user_id):
+    try:
+        user = User.query.get(user_id)
+        if not user:
+            return jsonify({"msg": f"User {user_id} does not exist"}), 400
+        
+        serialized_user = user.serialize()
+        return serialized_user, 200
+    except Exception as error:
+        return jsonify({"msg": "Server error", "error":str(error)}), 500
+    
 
-    response_body = {
-        "msg": "Hello, this is your GET /user response "
-    }
 
-    return jsonify(response_body), 200
+@app.route('/user', methods=['POST'])
+def create_a_user():
+    try:
+        request_body = json.loads(request.data)
+        new_User = User(
+            email = request_body["email"],
+            password = request_body["password"],
+            is_active = True
+        )
+        if not request_body:
+            return jsonify({"msg": "No information was provided"}), 400
+        
+        db.session.add(new_User)
+        db.session.commit()
+        serialized_user = new_User.serialize()
+
+        return jsonify({"msg": f"Created user: {serialized_user}"}), 200
+    
+
+    except Exception as error:
+        return jsonify({"msg": "Server error", "error":str(error)}), 500
+    
+
+#--------------------------PLANETS--------------------------------#
+
+#Getting the list of all planets within the system.
+@app.route('/planets', methods=['GET'])
+def get_all_planets():
+    try:
+        all_planets = Planet.query.all()
+        if len(all_planets) < 1:
+            return jsonify({"msg": "There are no planets registered in the platform."}), 400
+        
+        serialized_planets = list(map(lambda x: x.serialize(), all_planets))
+        return serialized_planets, 200
+    except Exception as error:
+        return jsonify({"msg": "Server error", "error":str(error)}), 500
+    
+#Getting the list of all planets within the system.
+@app.route('/planets/<int:planet_id>', methods=['GET'])
+def get_a_planet(planet_id):
+    try:
+        planet = Planet.query.get(planet_id)
+        if not planet:
+            return jsonify({"msg": f"The planet with id {planet_id} does not exist"}), 400
+        
+        serialized_planet = planet.serialize()
+        return serialized_planet, 200
+    except Exception as error:
+        return jsonify({"msg": "Server error", "error":str(error)}), 500
+
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
