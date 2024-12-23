72910709ded8c95d13611231fc90f24d9f5dc1aa
Lemonade <57147319+Lemonaeds@users.noreply.github.com>
2024-12-23 05:40:30 +0000

    Creacion de los modelos

--- src/models.py
+++ src/models.py
@@ -17,3 +17,81 @@ class User(db.Model):
             "email": self.email,
             # do not serialize the password, its a security breach
         }
+    
+#Creacion de la tabla de planetas
+class Planet(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(30), unique=True, nullable=False)
+    diameter = db.Column(db.String(10), unique=False, nullable=False)
+    rotation_period = db.Column(db.String(10), unique=False, nullable=False)
+    orbital_period = db.Column(db.String(10), unique=False, nullable=False)
+    population = db.Column(db.String(10), unique=False, nullable=False)
+    terrain = db.Column(db.String(10), unique=False, nullable=False)
+    surface_water = db.Column(db.String(10), unique=False, nullable=False)
+
+    def __repr__(self):
+        return '<Planet %r>' % self.id
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "diameter": self.diameter,
+            "rotation_period": self.rotation_period,
+            "orbital_period": self.orbital_period,
+            "population": self.population,
+            "terrain": self.terrain,
+            "surface_water": self.surface_water
+        }
+
+
+#Creacion de la tabla de personajes
+class Person(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(30), unique=True, nullable=False)
+    birth_year = db.Column(db.String(15), unique=False, nullable=False)
+    eye_color = db.Column(db.String(15), unique=False, nullable=False)
+    gender = db.Column(db.String(10), unique=False, nullable=False)
+    hair_color = db.Column(db.String(15), unique=False, nullable=False)
+    height = db.Column(db.String(3), unique=False, nullable=False)
+    height = db.Column(db.String(3), unique=False, nullable=False)
+    """ homeworld_id = db.Column(db.Integer, db.ForeignKey(Planet.id), nullable=False)
+    homeworld = db.relationship(Planet)
+    """
+    def __repr__(self):
+        return '<Person %r>' % self.id
+    
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "diameter": self.diameter,
+            "rotation_period": self.rotation_period,
+            "orbital_period": self.orbital_period,
+            "population": self.population,
+            "terrain": self.terrain,
+            "surface_water": self.surface_water,
+            "Planet":self.homeworld,
+            "planet_id": self.homeworld_id
+        }
+
+
+#Creacion de la tabla de favoritos
+class Favorites(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey(User.id), nullable=False)
+    user = db.relationship(User)
+    person_id = db.Column(db.Integer, db.ForeignKey(Person.id), nullable=True)
+    person = db.relationship(Person)
+    homeworld_id = db.Column(db.Integer, db.ForeignKey(Planet.id), nullable=True)
+    homeworld = db.relationship(Planet)
+
+    def __repr__(self):
+        return '<Favorites %r>' % self.id
+    
+    def serialize(self):
+        return {
+            "id": self.id,
+            "Person": self.person,
+            "Homeworld": self.homeworld,
+        }
